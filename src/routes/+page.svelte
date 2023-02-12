9f0ad9d3e2a5e42f84b040643db2ca129acd40d5
musyu36 <musyu36@gmail.com>
2023-02-12 15:11:45 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>TODO</h1>
+<a href="/todo/add">追加</a>
---
+++ src/routes/todo/add.svelte
@@ -0,0 +1,23 @@
+<script>
+    // 先頭が$のものはSvelteの組み込み　libは例外
+    import {goto} from '$app/navigation'
+    import * as api from '$lib/api_client.ts'
+
+    // プロパティ宣言
+    export let todo = "";
+
+    // TODOを新規登録する
+    async function submit(){
+        await api.post('/api/todo', {todo});
+        goto('/');
+    }
+</script>
+
+<h1>TODO追加</h1>
+
+<form on:submit|preventDefault={submit}>
+    <!-- bind:valueで入力値をプロパティーにバインドさせることで、スクリプト側で値を取得 -->
+    <input type="text" placeholder="TODO" bind:value={todo}>
+    <br>
+    <button>追加</button>
+</form>
